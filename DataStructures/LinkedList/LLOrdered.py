0337e1a7876a505977eae4c41a7866f26b1fa987
mca183 <mca183@sfu.ca>
2019-10-12 10:39:59 -0700

    making progress with Linked List

--- DataStructures/LinkedList/LLOrdered.py
+++ DataStructures/LinkedList/LLOrdered.py
@@ -1,24 +1,40 @@
-class Node:
-    def __init__(self, init_data):
-        self.data = init_data
-        self.next = None
+from Node import Node
+
+class OrderedList:
+    def __init__(self):
+        self.head = None
     
-    def get_data(self):
-        return self.data
-
-    def get_next(self):
-        return self.next
+    def search(self, item):
+        current = self.head
+        found = False
+        stop = False
+        while current != None and not found and not stop:
+            if current.get_data() == item:
+                found = True
+            else:
+                if current.get_data() > item:
+                    stop = True
+                else:
+                    current = current.get_next()
+        
+        return found
     
-    def set_data(self, new_data):
-        self.data = new_data
-
-    def set_next(self, new_next):
-        self.next = new_next
-
-
-if __name__ == '__main__':
-    temp = Node(93)
-    print(temp.get_data())
-    newNode = Node(100)
-    temp.set_next(newNode)
-    print(temp.get_next().get_data())
+    def add(self, item):
+        current = self.head
+        previous = None
+        stop = False
+        
+        while current != None and not stop:
+            if current.get_data() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.get_next()
+        
+        temp = Node(item)
+        if previous == None:
+            temp.set_next(self.head)
+            self.head = temp
+        else:
+            temp.set_next(current)
+            previous.set_next(temp)
--- DataStructures/LinkedList/LLUnordered.py
+++ DataStructures/LinkedList/LLUnordered.py
@@ -1,4 +1,4 @@
-from LLOrdered import *
+from Node import Node
 
 class UnorderedList:
     def __init__(self):
@@ -12,4 +12,65 @@ class UnorderedList:
         temp.set_next(self.head)
         self.head = temp
 
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count += 1
+            current = current.get_next()
+        
+        return count
+
+    def printLL(self):
+        current = self.head
+        result = ""
+        while current != None:
+            result += str(current.get_data()) + " -> "
+            current = current.get_next()
+
+        result += "Null"
+        return result
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                current = current.get_next()
+        
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+
+        while not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+        
+        if previous == None:
+            self.head = current.get_next()
+        else:
+            previous.set_text(current.get_next())
+
+
     
+if __name__ == '__main__':
+    mylist = UnorderedList()
+
+    mylist.add(31)
+    mylist.add(77)
+    mylist.add(17)
+    mylist.add(93)
+    mylist.add(26)
+    mylist.add(54)
+
+    print(mylist.printLL())
+
+    print(mylist.search(17))
